fdb9aa0ac205b9af808b8b18f92f04dbd5b57e8e
exyzte <davidecicerys@gmail.com>
2025-08-24 22:15:07 -0500

    connecting json file using .fetch api

--- travel_advisor.js
+++ travel_advisor.js
@@ -1,6 +1,34 @@
-const nav = document.querySelector('nav')
+const nav = document.querySelector('.navbar')
 fetch('./navbar.html')
 .then(response => response.text())
 .then(data=>{
     nav.innerHTML = data;
 })
+
+const search = document.querySelector('.search-input');
+const searchButton = document.querySelector('.search-button');
+const resultContainer = document.querySelector('.result-container');
+
+searchButton.addEventListener('click', () => {
+    fetch('travel_recommendation_api.json')
+    .then(response => response.json())
+    .then(data => {
+        const searchTerm = search.value.toLowerCase();
+        const result = data.filter(item =>
+            item.name.toLowerCase().includes(searchTerm) ||
+            item.description.toLowerCase().includes(searchTerm)
+        )
+        if (result.length === 0) {
+            resultContainer.innerHTML = '<p>No results found.</p>';
+            return;
+        } else {
+        resultContainer.innerHTML = result.map(item => 
+            `<div class="result-item">
+            <h3>${item.name}</h3>
+            <p>${item.description}</p>
+            <img src="${item.imageUrl}" alt="${item.name}">`
+        ).join('');
+    }
+
+    });
+});
